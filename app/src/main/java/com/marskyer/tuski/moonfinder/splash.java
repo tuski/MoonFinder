f8f78248d465dc796cc92a5bac9c3217905f71c8
tuski <tuski.ruet@gmail.com>
2016-04-16 20:18:35 +0600

    16/4/2016

---
+++ app/src/main/java/com/marskyer/tuski/moonfinder/splash.java
@@ -0,0 +1,69 @@
+package com.marskyer.tuski.moonfinder;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    int timer = 0;
+                    while (timer < 2000) {
+                        sleep(100);
+                        timer = timer + 100;
+                    }
+
+                    Intent openSplash = new Intent(splash.this, appmenu.class);
+                    startActivity(openSplash);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+
+    }
+}
+
