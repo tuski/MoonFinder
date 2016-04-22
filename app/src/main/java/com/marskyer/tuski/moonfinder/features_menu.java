d83552716846dab2dfbbbf7f48f0dbc5c8e0490e
mahedi kaysar <mahedi.kaysar@insight-centre.org>
2016-04-22 13:32:26 +0100

    Initial version of gallery activity added. Currently, It will show some static images.

--- app/src/main/java/com/marskyer/tuski/moonfinder/features_menu.java
+++ app/src/main/java/com/marskyer/tuski/moonfinder/features_menu.java
@@ -1,12 +1,15 @@
 package com.marskyer.tuski.moonfinder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class features_menu extends Activity {
 
@@ -20,9 +23,28 @@ public class features_menu extends Activity {
         setContentView(R.layout.activity_features_menu);
 
 
+        /*
+        added my @Mahedi
+         */
+        Button featureButton = (Button) findViewById(R.id.btn_pic);
+        featureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGalleryActivity();
+            }
+        });
+
 
     }
+    /*
+    added by @Mahedi
+     */
+    public void startGalleryActivity(){
+        Intent intent = new Intent(features_menu.this, GalleryActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getApplicationContext().startActivity(intent);
 
+    }
 
 
 }
