b6bf176682334a0c42224926f4b68d9957e1679e
tuski <tuski.ruet@gmail.com>
2016-04-22 16:47:43 +0600

    compass added

---
+++ app/src/main/java/com/marskyer/tuski/moonfinder/phase_anim.java
@@ -0,0 +1,24 @@
+package com.marskyer.tuski.moonfinder;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.InputStream;
+
+public class phase_anim extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phase_anim);
+
+
+
+
+    }
+
+
+}
