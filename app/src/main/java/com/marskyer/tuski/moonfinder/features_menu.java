140fdfdad5d009b567ede28e2d2256dc7fe25f8d
tuski <tuski.ruet@gmail.com>
2016-04-22 23:26:16 +0600

    compass added

--- app/src/main/java/com/marskyer/tuski/moonfinder/features_menu.java
+++ app/src/main/java/com/marskyer/tuski/moonfinder/features_menu.java
@@ -1,15 +1,18 @@
 package com.marskyer.tuski.moonfinder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class features_menu extends Activity {
-
+Button phs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //full screen
@@ -19,7 +22,19 @@ public class features_menu extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_features_menu);
 
+phs= (Button) findViewById(R.id.btn_phase);
 
+
+
+
+
+        phs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(features_menu.this, phase_anim.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
--- app/src/main/java/com/marskyer/tuski/moonfinder/phase_anim.java
+++ app/src/main/java/com/marskyer/tuski/moonfinder/phase_anim.java
@@ -1,7 +1,7 @@
 package com.marskyer.tuski.moonfinder;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,13 +9,14 @@ import android.view.MenuItem;
 import java.io.InputStream;
 
 public class phase_anim extends Activity {
+    GifView gifView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phase_anim);
 
-
+        gifView = (GifView) findViewById(R.id.gif_view);
 
 
     }
